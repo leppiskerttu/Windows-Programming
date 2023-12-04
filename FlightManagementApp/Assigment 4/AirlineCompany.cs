2086098f42592585730fee418c1e8aaf307d7a35
leppiskerttu <121945120+leppiskerttu@users.noreply.github.com>
2023-12-04 09:39:50 +0200

    Add files via upload

---
+++ FlightManagementApp/Assigment 4/AirlineCompany.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class AirlineCompany
+{
+    SortedList<Flight, double> flights = new SortedList<Flight, double>();
+    //sortedlistin key value on flight objecti ja double hinta
+
+    readonly string airlineName;
+
+
+    public AirlineCompany(string AirlineName)
+    {
+        this.airlineName = AirlineName;
+    }
+    public string AirlineName { get { return airlineName; } }
+   
+    //indexer mahdollistaa lentojen hakemisen indeksin kautta
+    public Flight this[int index]
+    {
+        get
+        {
+            
+
+            foreach (Flight flight in flights.Keys)
+            {
+                
+
+                if (flight.Id == index)
+                {
+                   
+
+                    return flight;
+                    
+                }
+            }
+
+          
+            return null;
+        }
+        set
+        {
+            
+            flights.Add(value, value.Price);
+        }
+    }
+
+
+
+    public Flight FindFlight(int flightId)
+    {
+        foreach (var flight in flights)
+            //käydään läpi jokainen variable flight joka on flights sortedlistissa
+        {
+            if (flight.Key.Id == flightId)
+                //jos flightin id on sama kuin etsittävä id
+            {
+                return flight.Key;
+               
+            }
+        }
+        return null;
+        
+    }
+
+    public Flight GetCheapestFlight()
+    {
+        return flights.Keys[0];
+        //palautetaan halvin lento
+    }
+
+    public Flight GetMostExpensiveFlight()
+    {
+        return flights.Keys[flights.Count - 1];
+        //palautetaan kallein lento
+    }
+
+    public string DisplayFlights()
+    {
+        StringBuilder allFlightsInfo = new StringBuilder();
+
+        foreach (var flight in flights.Keys)
+        {
+        allFlightsInfo.Append(flight.ToString()+Environment.NewLine);
+        }
+        return allFlightsInfo.ToString();
+    }
+
+}
---
+++ FlightManagementApp/Assigment 4/Flight.cs
@@ -0,0 +1,45 @@
+﻿public class Flight : IComparable<Flight>
+{
+    // Lentoluokan ominaisuudet, jotka kuvaavat lentotietoja.
+    public int Id { get; }
+    public string Origin { get; }
+    public string Destination { get; }
+    public DateTime Date { get; }
+    public double Price { get; }
+
+
+    public Flight() { }
+
+    public Flight(int id, string origin, string destination, DateTime date, double price)
+    {
+        // Konstruktori, joka asettaa lentotiedot luokan ominaisuuksiin.
+        Id = id;
+        Origin = origin;
+        Destination = destination;
+        Date = date;
+        Price = price;
+    }
+
+    public int CompareTo(Flight other)
+    {
+        if (other == null)
+            return 1;
+        // Jos verrattava lento on null, palautetaan arvo 1, mikä osoittaa, että tämä lento on suurempi.
+
+        if (this.Price > other.Price)
+            return 1;
+        else if (this.Price < other.Price)
+            return -1;
+        // Vertaillaan lentojen hintoja. Palautetaan 1, jos tämän lento on kalliimpi, -1 jos toinen lento on kalliimpi.
+
+        return 0;
+        // Jos hinnat ovat samat, palautetaan arvo 0 osoittaen, että lennot ovat yhtä suuret hinnan suhteen.
+    }
+
+
+    public override string ToString()
+    {
+        return $"Flight ID: {Id}\nOrigin: {Origin}\nDestination: {Destination}\nDate: {Date.ToString("MM/dd/yyyy")}\nPrice: {Price:C}\n";
+    }
+
+}
